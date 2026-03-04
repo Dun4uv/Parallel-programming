2d914b7e1c63060ddbfa2918a22b5a5b2540a6f9
Dun4uv <kekovka8@gmail.com>
2026-03-04 13:10:48 +0100

    Added graph and matrix generator and automatic launch with run_experiments.py, fixed README.md

--- lab1/src/main.cpp
+++ lab1/src/main.cpp
@@ -2,9 +2,9 @@
 #include <vector>
 #include <fstream>
 #include <chrono>
-#include <omp.h>
 
 using namespace std;
+
 using Matrix = vector<vector<double>>;
 
 Matrix readMatrix(const string& filename, int& N) {
@@ -36,13 +36,10 @@ void writeMatrix(const string& filename, const Matrix& M) {
     }
 }
 
-Matrix multiply(const Matrix& A, const Matrix& B, int threads) {
+Matrix multiply(const Matrix& A, const Matrix& B) {
     int N = A.size();
     Matrix C(N, vector<double>(N, 0.0));
 
-    omp_set_num_threads(threads);
-
-#pragma omp parallel for schedule(static)
     for (int i = 0; i < N; i++) {
         for (int k = 0; k < N; k++) {
             for (int j = 0; j < N; j++) {
@@ -50,15 +47,13 @@ Matrix multiply(const Matrix& A, const Matrix& B, int threads) {
             }
         }
     }
-
     return C;
 }
 
 int main(int argc, char* argv[]) {
-
-    if (argc != 5) {
+    if (argc != 4) {
         cout << "Использование:\n";
-        cout << "./matrix_mul A.txt B.txt result.txt threads\n";
+        cout << "matrix_mul.exe A.txt B.txt result.txt\n";
         return 1;
     }
 
@@ -72,10 +67,10 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    int threads = stoi(argv[4]);
-
     auto start = chrono::high_resolution_clock::now();
-    Matrix C = multiply(A, B, threads);
+
+    Matrix C = multiply(A, B);
+
     auto end = chrono::high_resolution_clock::now();
 
     chrono::duration<double> elapsed = end - start;
@@ -87,7 +82,6 @@ int main(int argc, char* argv[]) {
     cout << "Размер матрицы: " << N1 << "x" << N1 << '\n';
     cout << "Операции: " << operations << '\n';
     cout << "Время: " << elapsed.count() << " seconds\n";
-    cout << "Потоков: " << threads << '\n';
 
     return 0;
 }
